52b1d1cfa76dd2d9d3f0b58db39e4c6168333e71
Arhan Ansari <arhanansari2009@gmail.com>
2024-11-04 16:59:04 +0530

    Update GlobalApi.jsx

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 // const BASE_URL='http://localhost:1337/api';
-const BASE_URL='https://tubeguruji-admin.herokuapp.com/api'
+const BASE_URL='https://arhans-personal-blog.vercel.app/api'
 
 const getPost=axios.get(BASE_URL+'/blogs?populate=*');
 const getPostById=(id)=>axios.get(BASE_URL+'/blogs/'+id+'?populate=*');
