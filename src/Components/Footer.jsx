216292df4c406255bbbf85b2934ab9f059eb069b
Arhan Ansari <arhanansari2009@gmail.com>
2024-11-04 16:59:52 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='bg-gray-200 text-center'>
-        <h1 className='text-[14px] p-10'>Need help? Email hello@tubeguruji.com<br></br>
+        <h1 className='text-[14px] p-10'>Need help? Email: arhanansari2009@gmail.com<br></br>
 Copyright © 2024 Arhan Ansari</h1>
     </div>
   )
